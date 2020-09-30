e588d518a9b34bca3613fe20cb2c69220b967ad6
Andrey Fedorovich <a.fedorovich@pixelplex.by>
2020-09-30 22:57:56 +0300

    add contacts

--- src/hooks/use-body-class.hook.js
+++ src/hooks/use-body-class.hook.js
@@ -4,15 +4,12 @@ const addBodyClass = (className) => document.body.classList.add(className);
 const removeBodyClass = (className) => document.body.classList.remove(className);
 
 export default function useBodyClass(className) {
-  useEffect(
-    () => {
-      if (className instanceof Array) {
-        className.map(addBodyClass);
-        return () => className.map(removeBodyClass);
-      }
-      addBodyClass(className);
-      return () => removeBodyClass(className);
-    },
-    [className],
-  );
+  useEffect(() => {
+    if (className instanceof Array) {
+      className.map(addBodyClass);
+      return () => className.map(removeBodyClass);
+    }
+    addBodyClass(className);
+    return () => removeBodyClass(className);
+  }, [className]);
 }
